763d822dd6c2925c72479a9c618e27c2b5a36544
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-04-08 22:14:47 -0700

    adding drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,12 @@ var loadTasks = function() {
   // if nothing in localStorage, create a new object to track all task status arrays
   if (!tasks) {
     tasks = {
-      toDo: [],
+      toDo: [
+        // {
+        // text: "Sample text to do",
+        // date: "04/07/2020"
+        // }
+      ],
       inProgress: [],
       inReview: [],
       done: []
@@ -54,6 +59,69 @@ var saveTasks = function() {
 // $(".list-group").on("click", "p", function() {
 //   console.log(this);
 // });
+
+$(".card .list-group").sortable( {
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // console.log($(this).children());
+    // console.log("update", this);
+    // }
+    // loop over current set of children in sortable list
+
+    //array to store task data in
+    var tempArr = [];
+
+    //loop over current set of children in sortable list
+
+  $(this).children().each(function() {
+    console.log($(this));
+    var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+    var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+    //add task data to the temp array as an object
+    tempArr.push({
+      text: text,
+      date: date
+    })
+    // console.log(text, date);
+  });
+
+
+  //trim down list's ID to match object property
+  var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+  //update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+
+
+  }
+
+});
 
 
 $(".list-group").on("click", "p", function() {
@@ -91,7 +159,23 @@ $(".list-group").on("blur", "textarea", function() {
     .closest(".list-group-item")
     .index();
 
-    tasks[status][index].text = text;
+    //this part was added after working with JOHN
+    if(tasks[status].length == 0) {
+      tasks[status].push({text: text, date: "20500420"}
+      
+      )
+    }
+    else {
+      console.log(status);
+      console.log(index);
+      console.log(text);
+      console.log(tasks);
+  
+      // tasks[status][index].text = text;
+      tasks[status][index-1].text = text;
+    }
+
+   
     saveTasks();
 
 
@@ -204,6 +288,27 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+
+
+//functionality to remove tasks via drag and drop to trash
+//THIS IS VERY COOL REMOVE FEATURE BY DRAG AND DROP
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
+
+
 
 // load tasks for the first time
 loadTasks();
